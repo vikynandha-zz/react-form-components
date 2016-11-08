9cde889fd825f00293888c3612752269161a7885
Rohan Bagchi <rohan@recruiterbox.com>
2016-11-08 17:35:16 +0530

    Upgrade files for React 0.14

--- src/Form.js
+++ src/Form.js
@@ -1,10 +1,11 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
 var util = require('./util');
 
 var Form = React.createClass({
 
     onSubmit: function() {
-        this.getDOMNode().submit();
+        ReactDOM.findDOMNode(this).submit();
     },
 
     render: function() {
@@ -20,7 +21,7 @@ var Form = React.createClass({
         // In mobile, auto-focusing might look like too many transitions,
         // especially when the form is shown on a user action
         if(!this.props.preventAutoFocusOnMobile || !util.isMobile()) {
-            var form = this.getDOMNode();
+            var form = ReactDOM.findDOMNode(this);
             var autofocusField =
                     form.querySelector('[autofocus]:not([type="hidden"])') ||
                     form.querySelector('[data-autofocus]:not([type="hidden"])');
